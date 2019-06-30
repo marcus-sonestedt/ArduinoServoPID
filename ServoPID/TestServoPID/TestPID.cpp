f2040bc3fde99f6622e0c14b16d8726e1cc13557
Marcus Sonestedt <marcus.lindblom.sonestedt@bitaddict.se>
2019-06-30 21:33:47 +0200

    Check for zero in every loop

--- ServoPID/TestServoPID/TestPID.cpp
+++ ServoPID/TestServoPID/TestPID.cpp
@@ -5,10 +5,11 @@
 TEST(TestPID, StaysOnZero)
 {
     auto pid = PID(1.0f, 2.0f, 3.0f, 0.5f);
-    auto output = 0.0f;
-    for (auto i = 0; i < 10; ++i)
-        output = pid.regulate(42.0f, 42.0f, 1.0f);
-    ASSERT_FLOAT_EQ(output, 0.0f);
+
+    for (auto i = 0; i < 10; ++i) {
+        const auto output = pid.regulate(42.0f, 42.0f, 1.0f);
+        ASSERT_FLOAT_EQ(output, 0.0f);
+    }
 }
 
 TEST(TestPID, NoStartFlutter)
