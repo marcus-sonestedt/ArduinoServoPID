1034887b053c6a7aad3e7fe0fe2052c2f2803a65
Marcus Sonestedt <marcus.lindblom.sonestedt@bitaddict.se>
2019-07-05 20:15:08 +0200

    servopid: Start on C# controller app

---
+++ ServoPID/ServoPIDControl/Model.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServoPIDControl
+{
+    public class Model
+    {
+        public string PortName { get; set; } = "COM3";
+        public bool Enabled { get; set; } = true;
+        public ObservableCollection<ServoPidModel> Servos { get; set; } = new ObservableCollection<ServoPidModel>();
+    }
+}
---
+++ ServoPID/ServoPIDControl/ServoPidModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServoPIDControl
+{
+    public class ServoPidModel
+    {
+        public int Id { get; set; }
+        public float P { get; set; }
+        public float I { get; set; }
+        public float D { get; set; }
+        public float DLambda { get; set; } = 1.0f;
+
+        public float SetPoint { get; set; }
+        public float Input { get; set; }
+        public float Output { get; set; }
+
+        public float Integrator { get; set; }
+        public float DFiltered { get; set; }
+
+        public float InputScale { get; set; } = 1;
+        public float InputBias { get; set; } = 0;
+    }
+}
