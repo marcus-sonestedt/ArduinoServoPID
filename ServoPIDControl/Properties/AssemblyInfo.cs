c7b6991605008313ed2f05453df32681971fa6f4
Marcus Sonestedt <marcus.s.lindblom@gmail.com>
2020-04-19 14:37:04 +0200

    Failed attempt to work arund gitversion issue

--- ServoPIDControl/Properties/AssemblyInfo.cs
+++ ServoPIDControl/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ServoPIDControl")]
+[assembly: AssemblyTitle("Servo PID Control")]
 [assembly: AssemblyDescription("Control program for Arduino Servo PID")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Marcus Sonestedt")]
-[assembly: AssemblyProduct("Arduiono ServoPID Control")]
+[assembly: AssemblyProduct("Arduino ServoPID Control")]
 [assembly: AssemblyCopyright("Copyright © Marcus Sonestedt 2019-2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Windows;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-[assembly:NeutralResourcesLanguageAttribute("en-US")]
+//[assembly:NeutralResourcesLanguageAttribute("en-US")]1
 
 //In order to begin building localizable applications, set
 //<UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
@@ -31,7 +31,7 @@ using System.Windows;
 
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
-
+/*
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
                                      //(used if a resource is not found in the page,
@@ -40,7 +40,7 @@ using System.Windows;
                                               //(used if a resource is not found in the page,
                                               // app, or any theme specific resource dictionaries)
 )]
-
+*/
 
 // Version information for an assembly consists of the following four values:
 //
